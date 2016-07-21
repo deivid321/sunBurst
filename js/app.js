dd9427d3e74b3a0c32a8b37db6674964f37907ca
Deivydas <deivydas.voroneckis@cern.ch>
2016-07-21 11:29:58 +0200

    Added SunburstService into separate file.

--- js/app.js
+++ js/app.js
@@ -1,31 +1,140 @@
 var app = angular.module('ChartApp', []);
 
+/* DELETE THIS
 // We need to inject the $http service in to our factory
-app.factory("GetData",function($http) {
-  // Define the DribbblePlayer function
-  var DribbblePlayer = function(player) {
-    // Define the initialize function
-    this.initialize = function() {
-      // Fetch the player from Dribbble
-      var url = 'http://api.dribbble.com/players/' + player + '?callback=JSON_CALLBACK';
-      var playerData = $http.jsonp(url);
-      var self = this;
-      
-      // When our $http promise resolves
-      // Use angular.extend to extend 'this'
-      // with the properties of the response
-      playerData.then(function(response) {
-        angular.extend(self, response.data);  
-      });
+app.factory("DataFactory", function($http) {
+    return {
+        getData: function() {
+            return $http({
+                    method: "GET",
+                    url: "../MemoryJson.json"
+                }).then(function successRead(response) {
+                    var myWelcome = response.statusText;
+                    var entriesObj = response.data.histograms[0].total;
+                    var pathsObj = response.data.histograms[0].path;
+                    var entries = [];
+                    var paths = angular.fromJson(pathsObj);
+                    var paths2 = [];
+                    var textas = "";
+                    var i = 0;
+                    angular.forEach(entriesObj, function(item) {
+                        var a = parseInt(item, 10);
+                        entries.push(a);
+                    });
+                    angular.forEach(paths, function(item) {
+                        var item1 = item.toString();
+                        textas += "\n" + item1.replace(/\//g, '-') + ", " + entries[i]
+                        paths2.push(item1.replace(/\//g, '-') + ", " + entries[i]);
+                        i++;
+                    });
+                    // DataArray = paths;
+                    data = paths;
+                    //console.log(data);
+                     return response;
+                    //console.log(DataArray);
+
+                })
+                .error(function errorRead(response) {
+                    $scope.myWelcome = response.statusText;
+                    return response;
+                })
+                //return (DataArray);
+
+        }
+    }
+});
+*/
+
+/*//SHOULD BE WORKING BUT NOT
+app.service('DataService', function($http) {
+
+    var DataService = {
+        asyncc: function() {
+                var promise = $http({
+                    method: "GET",
+                    url: "../MemoryJson.json"
+                }).then(function(response) {
+                        var myWelcome = response.statusText;
+                        var entriesObj = response.data.histograms[0].total;
+                        var pathsObj = response.data.histograms[0].path;
+                        var entries = [];
+                        var paths = angular.fromJson(pathsObj);
+                        var paths2 = [];
+                        var textas = "";
+                        var i = 0;
+                        angular.forEach(entriesObj, function(item) {
+                            var a = parseInt(item, 10);
+                            entries.push(a);
+                        });
+                        angular.forEach(paths, function(item) {
+                            var item1 = item.toString();
+                            textas += "\n" + item1.replace(/\//g, '-') + ", " + entries[i]
+                            paths2.push(item1.replace(/\//g, '-') + ", " + entries[i]);
+                            i++;
+                        });
+                        // DataArray = paths;
+                        var datar = paths;
+                        //console.log(datar);
+                        return datar;
+                        //console.log(DataArray);
+
+                    },
+                    function(response) {
+                        var myWelcome = response.statusText;
+                    })
+                return promise;
+            }
+            //console.log(aa);
+            //datar = "fdsf";
+
+    };
+    return DataService;
+});
+*/
+/*//SHOULD BE ALSO FINE
+app.factory('dataService', function($http) {
+
+    var getData = function() {
+
+        return $http({
+            method: "GET",
+            url: "../MemoryJson.json"
+        }).then(function(response) {
+            var myWelcome = response.statusText;
+            var entriesObj = response.data.histograms[0].total;
+            var pathsObj = response.data.histograms[0].path;
+            var entries = [];
+            var paths = angular.fromJson(pathsObj);
+            var paths2 = [];
+            var textas = "";
+            var i = 0;
+            angular.forEach(entriesObj, function(item) {
+                var a = parseInt(item, 10);
+                entries.push(a);
+            });
+            angular.forEach(paths, function(item) {
+                var item1 = item.toString();
+                textas += "\n" + item1.replace(/\//g, '-') + ", " + entries[i]
+                paths2.push(item1.replace(/\//g, '-') + ", " + entries[i]);
+                i++;
+            });
+            // DataArray = paths;
+            console.log(textas);
+            return textas;
+            //console.log(DataArray);
+
+        });
     };
 
-    // Call the initialize function for every new instance
-    this.initialize();
-  };
-
-  // Return a reference to the function
-  return (DribbblePlayer);
+    return { getData: getData };
 });
+var myDataPromise = dataService.getData();
+    myDataPromise.then(function(response) {  
 
+       // this is only run after getData() resolves
+       $scope.data = response;
+       console.log("data.name"+$scope.data);
+    });
+    console.log("nothing");
 
-
+*/
