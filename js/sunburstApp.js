1cf090bd99f352e89d2fb635d230f225876449fd
Deivydas <deivydas.voroneckis@cern.ch>
2016-07-28 11:06:38 +0200

    Fixed firefox colors.

--- js/sunburstApp.js
+++ js/sunburstApp.js
@@ -388,7 +388,7 @@ function showSunburst($http) {
             var c = (i & 0x00FFFFFF)
                 .toString(16)
                 .toUpperCase();
-            return "00000".substring(0, 6 - c.length) + c;
+            return "#"+"00000".substring(0, 6 - c.length) + c;
         }
     }
 
